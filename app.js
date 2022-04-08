2873eb901d0e2f6328e65e96fbeddd11d6c3c60b
Ryani <ryanid.@Ryanis-MacBook-Air.local>
2022-04-08 11:17:29 -0400

    Added working refresh button; Edited id names for advice section

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const apiURL = 'https://api.adviceslip.com/advice';
 fetch(apiURL)
   .then(response => response.json())
   .then((data) => {
-    document.getElementById('adviceId').innerHTML = data.slip.id;
-    document.getElementById('adviceText').innerHTML = data.slip.advice;
+    document.getElementById('advice-id').innerHTML = data.slip.id;
+    document.getElementById('advice-text').innerHTML = data.slip.advice;
     console.log(data)}
     );
 
